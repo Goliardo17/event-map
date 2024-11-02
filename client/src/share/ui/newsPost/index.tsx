edf245b133a6921d99e2c9593d8e80b13ee10163
Goliardo17 <goliardo17@yandex.ru>
2024-11-02 14:01:50 +0300

    EM-8: Виджет - первая итерация NewsPost

--- client/src/share/ui/newsPost/index.tsx
+++ client/src/share/ui/newsPost/index.tsx
@@ -2,7 +2,7 @@ import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
-// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 type NewsProps = {
     title: string,
@@ -13,7 +13,7 @@ export const NewsPost: React.FC<NewsProps> = ({ title, body }) => {
     return (
         <Accordion>
             <AccordionSummary
-        //   expandIcon={<ExpandMoreIcon />}
+          expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1-content"
           id="panel1-header"
         >{title}</AccordionSummary>
